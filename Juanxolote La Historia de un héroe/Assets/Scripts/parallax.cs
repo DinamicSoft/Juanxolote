8be7316e3f8e0a1a8e95611f70a061efbfdd0298
Lui5-99 <41190188+Lui5-99@users.noreply.github.com>
2018-11-26 11:14:52 -0600

    Create parallax.cs
    Efecto parallax

---
+++ Juanxolote La Historia de un héroe/Assets/Scripts/parallax.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour {
+	
+	public float parallaxSpeed = 0.02f;
+	public RawImage fondo;
+	public RawImage suelo;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	float finalSpeed = parallaxSpeed * Time.deltaTime;
+	fondo.uvRect = new Rect(fondo.uvRect.x + finalSpeed,0f,1f,1f);
+	suelo.uvRect = new Rect(suelo.uvRect.x + finalSpeed *4 ,0f,1f,1f);
+		
+	}
+}
